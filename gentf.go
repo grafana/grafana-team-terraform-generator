30fc145c10fd6668a9877fb1099e004f2181a163
jguer <me@jguer.space>
2024-07-12 14:42:32 +0200

    add outputs generator

--- gentf.go
+++ gentf.go
@@ -35,3 +35,34 @@ resource "grafana_team_external_group" "%s_group" {
 
 	return nil
 }
+
+func generateOutputsFile(filepath string, groups []Group) error {
+	file, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Write the opening of the outputs block
+	_, err = file.WriteString("output \"team_ids\" {\n  value = {\n")
+	if err != nil {
+		return err
+	}
+
+	// Generate an output for each team
+	for _, group := range groups {
+		resourceName := strings.ReplaceAll(strings.ToLower(group.Name), " ", "_")
+		_, err = file.WriteString(fmt.Sprintf("    %s = grafana_team.%s.id\n", resourceName, resourceName))
+		if err != nil {
+			return err
+		}
+	}
+
+	// Write the closing of the outputs block
+	_, err = file.WriteString("  }\n  description = \"Map of team names to their IDs\"\n}\n")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -18,6 +18,8 @@ type fetchGroupsResult struct {
 	NextLink *string
 }
 
+var cachedGroups []Group // Define a global variable to hold the groups as a cache
+
 func main() {
 	// Set up Viper configuration
 	err := setupConfig()
@@ -32,21 +34,41 @@ func main() {
 		slog.SetLogLoggerLevel(slog.LevelDebug)
 	}
 
-	// Get the selected provider
-	provider := viper.GetString("provider")
-
 	baseDir := "grafana_tf"
-	if len(os.Args) > 1 && os.Args[1] == "init" {
-		slog.Info("Initializing basic Terraform template")
-		err := createGrafanaTerraformStructure(baseDir)
-		if err != nil {
-			slog.Error("Failed to create Terraform structure", "error", err)
-			os.Exit(1)
-		}
-		return
+	opt := "team"
+	if len(os.Args) > 1 {
+		opt = os.Args[1]
 	}
 
+	var errCmd error
+	switch opt {
+	case "team":
+		slog.Info("Generating Terraform file for teams")
+		errCmd = genTeamResources(baseDir)
+	case "team-outputs":
+		slog.Info("Generating Terraform file for teams")
+		errCmd = genTeamResourcesOutput(baseDir)
+	case "init":
+		slog.Info("Initializing basic Terraform template")
+		errCmd = createGrafanaTerraformStructure(baseDir)
+	}
+
+	if errCmd != nil {
+		slog.Error("Failed to execute command", "error", errCmd)
+		os.Exit(1)
+	}
+}
+
+func fetchGroups() ([]Group, error) {
+	if len(cachedGroups) > 0 {
+		slog.Debug("Using cached groups")
+		return cachedGroups, nil
+	}
+
+	// Get the selected provider
+	provider := viper.GetString("provider")
 	var groups []Group
+	var err error
 
 	switch provider {
 	case "azure":
@@ -62,7 +84,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	teamFile := "main.tf"
+	return groups, nil
+}
+
+func genTeamResourcesOutput(baseDir string) error {
+	teamOutputsFile := "teams-output.tf"
+	// if inited with `init` command, the file will be created in grafana_tf directory
+	if _, err := os.Stat(baseDir); err == nil {
+		teamOutputsFile = baseDir + "/modules/teams/outputs.tf"
+	}
+
+	if _, err := os.Stat(teamOutputsFile); err == nil {
+		slog.Warn("Terraform file already exists, overwriting", "file", teamOutputsFile)
+	}
+
+	groups, err := fetchGroups()
+	if err != nil {
+		return err
+	}
+
+	// Generate Terraform file
+	if err = generateOutputsFile(teamOutputsFile, groups); err != nil {
+		slog.Error("Failed to generate Terraform file", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("Terraform file generated successfully", "file", teamOutputsFile)
+
+	return nil
+}
+
+func genTeamResources(baseDir string) error {
+	teamFile := "teams.tf"
 	// if inited with `init` command, the file will be created in grafana_tf directory
 	if _, err := os.Stat(baseDir); err == nil {
 		teamFile = baseDir + "/modules/teams/main.tf"
@@ -72,14 +125,20 @@ func main() {
 		slog.Warn("Terraform file already exists, overwriting", "file", teamFile)
 	}
 
-	// Generate Terraform file
-	err = generateTerraformFile(teamFile, groups)
+	groups, err := fetchGroups()
 	if err != nil {
+		return err
+	}
+
+	// Generate Terraform file
+	if err = generateTerraformFile(teamFile, groups); err != nil {
 		slog.Error("Failed to generate Terraform file", "error", err)
 		os.Exit(1)
 	}
 
 	slog.Info("Terraform file generated successfully", "file", teamFile)
+
+	return nil
 }
 
 func setupConfig() error {
